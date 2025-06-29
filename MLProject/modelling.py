ab6811b2d436e39b9c9e0817e4ee74b5e274f4dc
Akbar Nur Rizqi <111796035+akbarnurrizqi167@users.noreply.github.com>
2025-06-29 23:58:07 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -12,8 +12,8 @@ mlflow.sklearn.autolog()
 df = pd.read_csv("dataset_preprocessing/train.csv")
 
 # Pisahkan fitur dan target
-X_train = train_df.drop(columns=["Churn"])
-y_train = train_df["Churn"]
+X_train = df.drop(columns=["Churn"])
+y_train = df["Churn"]
 
 # Split untuk validasi internal
 X_train, X_val, y_train, y_val = train_test_split(
